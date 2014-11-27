79bad05c8d5b6d3ea64059e55c1eab884adc6103
davidhan527 <davidhan527@gmail.com>
2014-11-27 12:20:01 -0500

    adding links to layout file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def about
+
+    render 'about.html.erb'
   end
 
   def contact
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/about'
+  get 'pages/about' => 'pages#about'
 
   get 'pages/contact'
 
