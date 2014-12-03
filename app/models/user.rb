35646800487e3fcb0198a617224cb3c9d9d59bb2
Paul Han <paulhan221@gmail.com>
2014-12-03 15:21:18 -0500

    linking reviews to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :reviews, dependent: :destroy
+  #just means if user is destroyed, all reviews should be destroyed as well
 end
